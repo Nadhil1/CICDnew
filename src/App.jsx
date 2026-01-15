6ef3ef493bd359c6db5142605a87aea86e510213
Nadhil Farzeen <farzeennadhil93@gmail.com>
2026-01-15 14:12:40 +0530

    Fix typo in edit instruction text

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMRvhgchgvchghgvhgvhdascdsvg
+          Edit <code>src/App.jsx</code> and save to test HMRvhgchgvchghgvhgvhdascdsvgcdcsdc
         </p>
       </div>
       <p className="read-the-docs">
